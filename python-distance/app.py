51ee46b4eba027b8b0af782927326a15820ffca0
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2018-02-26 10:44:22 -0500

    Include health check for database, should be disconnected.

--- python-distance/app.py
+++ python-distance/app.py
@@ -1,9 +1,45 @@
 from flask import Flask, jsonify, request
+import pymongo
 import os
 import logging
 import requests
 
 headers = {'Content-Type': 'application/json'}
+
+class DistanceError(Exception):
+    status_code = 500
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv = self.message
+        return rv
+
+class DistanceHealthStatus():
+    def __init__(self):
+        self.database = 'connected'
+        self.stars = 'connected'
+
+    def check_stars(self):
+        try: 
+            r = requests.get(app.config['STARS_ENDPOINT']) 
+        except requests.exceptions.ConnectionError as e:
+            logging.error(e)
+            self.stars = 'disconnected'
+    
+    def check_database(self):
+        try:
+            client = pymongo.MongoClient(os.getenv('DB_URI', 'mongodb://localhost:27017'),
+                                        serverSelectionTimeoutMS=100).database_names()
+        except pymongo.errors.ServerSelectionTimeoutError as e:
+            logging.error(e)
+            self.database = 'disconnected'
 
 app = Flask(__name__)
 try:
@@ -12,16 +48,24 @@ try:
 except Exception as e:
     logging.error(e)
 
+@app.errorhandler(DistanceError)
+def handle_distance_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
 @app.route('/')
 def index():
-    return 'From a distance...'
+    return 'Get sum of all distances with /distance.'
 
-@app.route('/health')
+@app.route('/distance/health')
 def health():
-    r = requests.get(app.config['STARS_ENDPOINT'])
-    if r.status_code > 499:
-        return jsonify({'stars': 'unhealthy'})
-    return jsonify({'stars': 'healthy'})
+    health_status = DistanceHealthStatus()
+    health_status.check_database()
+    health_status.check_stars()
+    if 'disconnected' in health_status.stars:
+        raise DistanceError(health_status.__dict__, status_code=500)
+    return jsonify(health_status.__dict__)
 
 @app.route('/distance')
 def sum_distance():
@@ -37,4 +81,4 @@ def sum_distance():
     return jsonify({'result' : sum_distance})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=app.config['APP_PORT'], debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv('APP_PORT', 80)), debug=True)
--- python-distance/test_app.py
+++ python-distance/test_app.py
@@ -8,10 +8,10 @@ url = 'http://localhost:5000'
 star = {'name': 'world', 'distance': 200}
 headers = {'Content-Type': 'application/json'}
 
-def test_should_return_healthy():
-    r = requests.get(url + '/health')
-    r.raise_for_status()
-    assert r.json() == {'stars': 'healthy'}
+def test_should_return_connected_to_stars_only():
+    r = requests.get(url + '/distance/health')
+    assert r.status_code == 200
+    assert r.json() == {'stars': 'connected', 'database': 'disconnected'}
 
 def test_should_return_sum_of_distance():
     data = json.dumps(star)
