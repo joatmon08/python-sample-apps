d1ccd480e8c3aa01e8c0493395615e3d434a8da0
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2018-04-09 19:26:52 -0400

    Change simple Python application to port 80 by default. Remove /hello root path.

--- python-simple/app.py
+++ python-simple/app.py
@@ -5,16 +5,16 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/')
 def hello():
     return 'Hello World!'
 
 
-@app.route('/hello/another')
+@app.route('/another')
 def test():
 	user = request.args.get('user')
 	return user + ' says Hello!'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv('APP_PORT', 8080)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv('APP_PORT', 80)), debug=True)
