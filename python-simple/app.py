f7eaf1566025bddbc1274df16439e6db80896704
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2018-04-09 18:39:12 -0400

    Reset testapp API endpoints for nginx reverse proxy example. Include makefile to build and push the image.

--- python-simple/app.py
+++ python-simple/app.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask import request
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/hello')
 def hello():
     return 'Hello World!'
 
-@app.route('/another')
+
+@app.route('/hello/another')
 def test():
 	user = request.args.get('user')
 	return user + ' says Hello!'
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv('APP_PORT', 8080)), debug=True)
